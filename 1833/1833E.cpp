c6e2201e43d678e6819165dd665b187795cc5024
David <65980127+dyang5@users.noreply.github.com>
2023-05-24 17:28:04 -0700

    Solved 1833E.

--- 1833/1833E.cpp
+++ 1833/1833E.cpp
@@ -1,17 +1,78 @@
 #include <bits/stdc++.h>
 #include <iostream>
+#include <set>
+#include <queue>
 
 using namespace std;
 
 void solve() {
     int n; cin >> n;
     
-    vector<vector<int>> adjList(n, vector<int>());
+    vector<set<int> > adjList(n);
 
     for (int i = 0 ; i < n; i++) {
-        int val; cin >> val;
-        adjList[i].push_back(val - 1);
+        int val; cin >> val; val -= 1;
+        adjList[i].insert(val);
+        adjList[val].insert(i);
     }
+
+    vector<int> degree(n);
+
+    for (int i = 0 ; i < n; i++) {
+        degree[i] = adjList[i].size();
+    }
+
+    
+    bool visited[n];
+
+    for (int i = 0 ; i < n; i++) {
+        visited[i] = false;
+    }
+ 
+    int bamboos = 0; int cycles = 0;
+
+    for (int i = 0 ; i < n; i++) {
+        if (!visited[i]) {
+            queue<int> vertices;
+
+            visited[i] = true;
+            vertices.push(i);
+
+            vector<int> component;
+            component.push_back(i);
+
+            while (!vertices.empty()) {
+                int cur = vertices.front(); vertices.pop();
+
+                for (auto &next : adjList[cur]) {
+                    if (!visited[next]) {
+                        visited[next] = true;
+                        vertices.push(next);
+                        component.push_back(next);
+                    }
+                }
+            }
+
+            bool bamboo = false;
+            for (auto &j: component) {
+                if (degree[j] == 1) {
+                    bamboo = true;
+                    break;
+                }
+            }
+
+            if (bamboo) {
+                bamboos++;
+            }
+
+            else {
+                cycles++;
+            }
+        }
+    }
+
+    cout << min(bamboos, 1) + cycles << " " << bamboos + cycles << endl;
+    
 }
 
 int main() {
