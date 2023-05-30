bf0c9b4b81a87ec98428d1312f90e6aa15aa11f4
David <65980127+dyang5@users.noreply.github.com>
2023-05-30 15:33:39 -0700

    Solved 1829G.

--- 1829/1829G.cpp
+++ 1829/1829G.cpp
@@ -5,13 +5,29 @@
 
 using namespace std;
 
+long long ans[1000005];
+long long pre[1500][1500] = {};
+
 void solve() {
     int n; cin >> n;
-
-    // use DP? 
+    cout << ans[n] << endl;
 }
 
 int main() {
+    long long curr = 1;
+
+    for (int i = 1; i < 1500; i++) {
+        for (int j = i - 1; j >= 1; j--) {
+            pre[j][i-j] = pre[j-1][i-j] + pre[j][i-j-1] - pre[j-1][i-j-1] + curr*curr;
+            ans[curr] = pre[j][i-j];
+            curr++;
+
+            if (curr > 1e6 + 2) {
+                break;
+            }
+        }
+    }
+
     int T;
     cin >> T;
 
